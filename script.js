f1c26c4284d2a04696915cfc3f953dd8a170097f
melissalopesbueno <106812898+melissalopesbueno@users.noreply.github.com>
2024-09-04 22:55:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,67 +6,69 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
    {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você acha o uso dessa tecnologia pode impactar o mercado de trabalho?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Acho que poderia ajudar de maneira positiva.",
+                afirmacao: "A pode automatizar tarefas repetitivas, permitindo que os trabalhadores se concentrem em atividades mais criativas e estratégicas."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Não acho que seria bom impacto ao mercado de trabalho",
+                afirmacao: "A IA pode levar à perda de empregos em setores onde a automação substitui a mão de obra humana."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "A IA poderia beneficiar a área da sáude?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Sim, séria uma boa ajuda.",
+                afirmacao: "A IA pode ajudar a diagnosticar doenças precocemente e personalizar tratamentos para pacientes."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Não acho que séria muito confiavel.",
+                afirmacao: " A IA pode levantar preocupações sobre privacidade e segurança dos dados dos pacientes.
+"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado:  "A IA poderia nos ajudar na educação?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Sim, facilitaria muito a forma de estudo.",
+                afirmacao: "A IA pode personalizar o aprendizado para atender às necessidades individuais dos alunos, tornando o ensino mais eficaz."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Não séria uma forma de estudo tão eficas.",
+                afirmacao: "A IA pode reduzir a interação humana entre professores e alunos, o que é essencial para o desenvolvimento social.
+"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "A IA é segura para a segurança cibernética?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Sim, ajudaria muito a evitar esse tipo de problema. ",
+                afirmacao: " A IA pode detectar e responder a ameaças cibernéticas mais rapidamente do que os métodos tradicionais."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Não, poderia tornar esse problema mais perigoso.",
+                afirmacao: "A IA também pode ser usada por hackers para criar ataques cibernéticos mais sofisticados."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "A IA pode influenciar as decisões de empresas?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Sim, ajudaria com decisões importantes.",
+                afirmacao: "A IA pode analisar grandes volumes de dados para fornecer insights valiosos e apoiar decisões estratégicas."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Não, poderia levar a empresa em uma situação complicada. ",
+                afirmacao: "A IA pode levar a decisões enviesadas se os dados utilizados forem tendenciosos ou incompletos."
             }
         ]
     },
