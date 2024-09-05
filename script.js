b926a26e38489b879a030049c85520d45e6afe2c
melissalopesbueno <106812898+melissalopesbueno@users.noreply.github.com>
2024-09-04 23:00:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-altenativas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
@@ -73,6 +73,8 @@ const perguntas = [
         ]
     },
 ];
+
+
 
 let atual = 0;
 let perguntaAtual;
